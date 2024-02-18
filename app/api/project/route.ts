a53d6a98356a72d932e73bfc9ea1cd8844e1e4db
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2024-02-18 00:57:58 -0300

    project put

--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -1,4 +1,4 @@
-import { created, serverError } from "@/lib/http-response";
+import { created, ok, serverError } from "@/lib/http-response";
 import { log } from "@/lib/log";
 import { prisma } from "@/lib/prisma";
 import { Project } from "@/lib/types";
@@ -11,6 +11,25 @@ export async function POST(req: Request) {
     });
 
     return created(create);
+  } catch (error) {
+    log(error);
+    return serverError();
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const parsed = Project.pick({ id: true, title: true }).parse(
+      await req.json()
+    );
+    const updated = await prisma.project.update({
+      where: {
+        id: parsed.id,
+      },
+      data: parsed,
+    });
+
+    return ok(updated);
   } catch (error) {
     log(error);
     return serverError();
