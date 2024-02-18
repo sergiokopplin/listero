0b87c41cc957642a2297872f441cb38c572d5cfe
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2024-02-18 00:41:50 -0300

    get task

---
+++ app/api/task/[id]/route.ts
@@ -0,0 +1,26 @@
+import { notFound, ok, serverError } from "@/lib/http-response";
+import { log } from "@/lib/log";
+import { prisma } from "@/lib/prisma";
+import { Task } from "@/lib/types";
+
+export async function GET(_: Request, context: { params: { id: number } }) {
+  try {
+    const parsed = Task.pick({ id: true }).parse({
+      id: Number(context.params.id),
+    });
+    const selected = await prisma.task.findFirst({
+      where: {
+        id: parsed.id,
+      },
+    });
+
+    if (!selected) {
+      return notFound();
+    }
+
+    return ok(selected);
+  } catch (error) {
+    log(error);
+    return serverError();
+  }
+}
